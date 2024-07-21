251455b0ac7c532198ab570a7becf43864a6b474
park20011029 <rokmck156@gmail.com>
2024-07-21 16:10:11 +0900

    Fix: Fix 로그인 로직 버그 수정

--- campton/campton/src/main/java/com/dongguk/campton/dto/request/SigninRequestDto.java
+++ campton/campton/src/main/java/com/dongguk/campton/dto/request/SigninRequestDto.java
@@ -2,9 +2,11 @@ package com.dongguk.campton.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SigninRequestDto {
     private String memberLoginId;
     private String memberPassword;
--- campton/campton/src/main/java/com/dongguk/campton/dto/response/SigninResponseDto.java
+++ campton/campton/src/main/java/com/dongguk/campton/dto/response/SigninResponseDto.java
@@ -2,9 +2,13 @@ package com.dongguk.campton.dto.response;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Builder
+@Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class SigninResponseDto {
     private Long id;
     private String name;
